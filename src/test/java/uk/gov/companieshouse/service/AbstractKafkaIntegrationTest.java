19fe04f4cdd77b2f2bec5be886e3bc355d126493
tombradley1 <tbradley@companieshouse.gov.uk>
2026-04-29 09:03:19 +0100

    Use kraft over zookeeper for testcontainer kafka

--- src/test/java/uk/gov/companieshouse/service/AbstractKafkaIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/service/AbstractKafkaIntegrationTest.java
@@ -15,7 +15,7 @@ public abstract class AbstractKafkaIntegrationTest {
 
     @Container
     protected static final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse(
-            "confluentinc/cp-kafka:5.0.0"));
+            "confluentinc/cp-kafka:5.0.0")).withKraft();
 
     @DynamicPropertySource
     static void props(DynamicPropertyRegistry registry) {
