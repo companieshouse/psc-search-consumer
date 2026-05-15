e458f9573133426cbf594c38e6946ec7de594814
rmcmurray-ch <rmcmurray@companieshouse.gov.uk>
2026-05-15 16:42:57 +0100

    removed unused import

--- src/test/java/uk/gov/companieshouse/resourcechanged/service/PscSearchUpsertServiceTest.java
+++ src/test/java/uk/gov/companieshouse/resourcechanged/service/PscSearchUpsertServiceTest.java
@@ -5,7 +5,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
