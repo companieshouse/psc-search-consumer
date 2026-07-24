16a6dd210419dfe113b71b942495a481cec3cee3
brdywltn <bwilton@companieshouse.gov.uk>
2026-07-24 11:08:47 +0100

    Implement invalidPayloadException

--- src/main/java/uk/gov/companieshouse/common/exception/InvalidPayloadException.java
+++ src/main/java/uk/gov/companieshouse/common/exception/InvalidPayloadException.java
@@ -1,7 +1,8 @@
 package uk.gov.companieshouse.common.exception;
 
 public class InvalidPayloadException extends RuntimeException {
-  public InvalidPayloadException(String message) {
-    super(message);
-  }
+
+    public InvalidPayloadException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
