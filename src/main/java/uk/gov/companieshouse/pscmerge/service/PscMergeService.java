7f4aa4358f6dbdc83923121907c85a2df0ab9b09
aokunade <aokunade@companieshouse.gov.uk>
2026-08-07 16:37:34 +0100

    Map NotificationList to PscList before updating search index

--- src/main/java/uk/gov/companieshouse/pscmerge/service/PscMergeService.java
+++ src/main/java/uk/gov/companieshouse/pscmerge/service/PscMergeService.java
@@ -1,7 +1,11 @@
 package uk.gov.companieshouse.pscmerge.service;
 
+import java.util.Collections;
+
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+
+import uk.gov.companieshouse.api.psc.PscList;
 import uk.gov.companieshouse.common.client.NotificationsApiClient;
 import uk.gov.companieshouse.common.client.PrimarySearchApiClient;
 import uk.gov.companieshouse.common.logging.DataMapHolder;
@@ -29,13 +33,21 @@ public class PscMergeService implements MergeService {
         final String previousPscId = pscMerge.getPreviousPscId();
         DataMapHolder.get()
                 .pscId(pscMerge.getPscId())
-                .previousOfficerId(previousPscId);
-        notificationsApiClient.getPscAppointmentListForDelete(URI.formatted(previousPscId))
-                .ifPresentOrElse(pscList -> {
+                .previousPscId(previousPscId);
+        notificationsApiClient.getPscNotificationListForDelete(URI.formatted(previousPscId))
+                .ifPresentOrElse(notificationList -> {
                     LOGGER.info("Updating previous notification in index", DataMapHolder.getLogMap());
+                    PscList pscList = new PscList();
+                    pscList.setActiveCount(notificationList.getActiveCount());
+                    pscList.setCeasedCount(notificationList.getCeasedCount());
+                    pscList.setItemsPerPage(notificationList.getItemsPerPage());
+                    pscList.setStartIndex(notificationList.getStartIndex());
+                    pscList.setTotalResults(notificationList.getTotalResults());
+                    pscList.setLinks(notificationList.getLinks());
+                    pscList.setItems(Collections.emptyList());
                     primarySearchApiClient.upsertPsc(previousPscId, pscList);
                 }, () -> {
-                   LOGGER.info("Deleting previous officer from index", DataMapHolder.getLogMap());
+                   LOGGER.info("Deleting previous psc from index", DataMapHolder.getLogMap());
                    primarySearchApiClient.deletePsc(previousPscId);
                 });
     }
