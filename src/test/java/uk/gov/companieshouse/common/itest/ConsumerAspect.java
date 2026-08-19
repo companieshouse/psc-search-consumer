8bd382e93dacb50fb5ac8bfd05ceb74e0bb86fb3
brdywltn <bwilton@companieshouse.gov.uk>
2026-08-19 17:03:54 +0100

    Implement getLatch, resetLAtch and modify the afterConsume to check for PscMergeConsumer messages

--- src/test/java/uk/gov/companieshouse/common/itest/ConsumerAspect.java
+++ src/test/java/uk/gov/companieshouse/common/itest/ConsumerAspect.java
@@ -4,20 +4,28 @@ import java.util.concurrent.CountDownLatch;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class ConsumerAspect {
 
+    private final int steps;
     private CountDownLatch latch;
 
-    public ConsumerAspect(CountDownLatch latch) {
-        this.latch = latch;
+    public ConsumerAspect(@Value("${steps:1}") int steps) {
+        this.steps = steps;
+        this.latch = new CountDownLatch(steps);
     }
 
-    @After("execution(* uk.gov.companieshouse.resourcechanged.ResourceChangedConsumer.consume(..))")
+    @After("execution(* uk.gov.companieshouse.resourcechanged.ResourceChangedConsumer.consume(..)) "
+            + "|| execution(* uk.gov.companieshouse.pscmerge.PscMergeConsumer.consume(..))")
     void afterConsume(JoinPoint joinPoint) {
         latch.countDown();
     }
+
+    public CountDownLatch getLatch() { return latch; }
+
+    public void resetLatch() { latch = new CountDownLatch(steps); }
 }
